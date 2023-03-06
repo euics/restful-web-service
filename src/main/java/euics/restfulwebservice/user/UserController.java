faf63bb2b193bd69bc9423cf59dbe1d02cd277f1
JongHun <pjh575157@gmail.com>
2023-03-06 23:03:20 +0900

    2023/03/06 GET HTTP Method를 통한 사용자 목록 조회 API 구현

---
+++ src/main/java/euics/restfulwebservice/user/UserController.java
@@ -0,0 +1,27 @@
+package euics.restfulwebservice.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserDaoService service;
+
+    public UserController(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+
+    // GET /users/1 or /users/10 -> String 형태로 전달
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id){
+        return service.findOne(id);
+    }
+}
