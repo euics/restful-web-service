f9221583ae1a25053c8fff531407cf03b769ac9a
JongHun <pjh575157@gmail.com>
2023-03-06 22:07:35 +0900

    2023/03/06 Initial Commit

---
+++ src/test/java/euics/restfulwebservice/RestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package euics.restfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
