66958016fce92335aa950eb86d0208b6d3f32eb8
JongHun <pjh575157@gmail.com>
2023-03-06 23:36:49 +0900

    2023/03/06 HTTP Status Code 제어를 위한 Exception Handling

---
+++ src/main/java/euics/restfulwebservice/user/UserNotFoundException.java
@@ -0,0 +1,17 @@
+package euics.restfulwebservice.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/*
+ HTTP Status Code
+ 2XX -> OK
+ 4XX -> Client
+ 5XX -> Server
+*/
+@ResponseStatus(HttpStatus.NOT_FOUND) // 500 Error가 아닌 NOT_FOUND 오류를 뜨게 만듭니다.
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
