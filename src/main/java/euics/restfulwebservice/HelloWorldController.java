5be818956751ddcb01aec12ac684a8f565681b58
JongHun <pjh575157@gmail.com>
2023-03-06 22:09:13 +0900

    2023/03/06 hello-world, hello-world-bean, PathVariable URL을 갖는 Controller 생성

---
+++ src/main/java/euics/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,28 @@
+package euics.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    /*
+     GET 방식
+     URL: /hello-world (endpoint)
+     @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    */
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping("/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format(name));
+    }
+}
