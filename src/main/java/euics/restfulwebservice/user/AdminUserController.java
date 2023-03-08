2ee8254a9d4eee6cfe72216195ce6ebab8a1a437
JongHun <pjh575157@gmail.com>
2023-03-08 18:13:10 +0900

    2023/03/08 URI를 이용한 REST API Version 관리

--- src/main/java/euics/restfulwebservice/user/AdminUserController.java
+++ src/main/java/euics/restfulwebservice/user/AdminUserController.java
@@ -3,6 +3,7 @@ package euics.restfulwebservice.user;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,9 +34,9 @@ public class AdminUserController {
         return mapping;
     }
 
-    // GET /users/1 or /users/10 -> String 형태로 전달
-    @GetMapping("/users/{id}")
-    public MappingJacksonValue retrieveUser(@PathVariable int id){
+    // GET /admin/users/1 -> /admin/v1/users/1
+    @GetMapping("/v1/users/{id}")
+    public MappingJacksonValue retrieveUserV1(@PathVariable int id){
         User user = service.findOne(id);
 
         if(user == null)
@@ -48,6 +49,32 @@ public class AdminUserController {
         FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
 
         MappingJacksonValue mapping = new MappingJacksonValue(user);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+    // GET /admin/users/1 -> /admin/v2/users/1
+    @GetMapping("/v2/users/{id}")
+    public MappingJacksonValue retrieveUserV2(@PathVariable int id){
+        User user = service.findOne(id);
+
+        if(user == null)
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+
+        // User -> User2
+        UserV2 userV2 = new UserV2();
+        BeanUtils.copyProperties(user, userV2);
+        userV2.setGrade("VIP");
+
+        // Filtering 생성하기
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate", "grade");
+
+        // Filtering을 UserInfoV2로 설정
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfoV2", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(userV2);
         mapping.setFilters(filters);
 
         return mapping;
