05f4436708be8ce7c697a51dc290ac1ace63ba2a
JongHun <pjh575157@gmail.com>
2023-03-10 23:55:24 +0900

    2023/03/10 새로운 사용자 생성 시 primary key의 오류로 인해 더미 데이터 id 값 조정

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-insert into users values(0, now(), 'user1', 'password1', 'ssn1');
-insert into users values(1, now(), 'user2', 'password2', 'ssn2');
-insert into users values(2, now(), 'user3', 'password3', 'ssn3');
+insert into users values(90000, now(), 'user1', 'password1', 'ssn1');
+insert into users values(90001, now(), 'user2', 'password2', 'ssn2');
+insert into users values(90002, now(), 'user3', 'password3', 'ssn3');
