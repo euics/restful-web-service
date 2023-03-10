b7e250c5fa4437c2c4c77e96bef27451e601d35f
JongHun <pjh575157@gmail.com>
2023-03-10 23:09:14 +0900

    2023/03/10 Spring Data JPA를 이용한 초기 더미 데이터 생성

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into users values(0, now(), 'user1', 'password1', 'ssn1');
+insert into users values(1, now(), 'user2', 'password2', 'ssn2');
+insert into users values(2, now(), 'user3', 'password3', 'ssn3');
