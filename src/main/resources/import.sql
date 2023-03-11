406f56948dc83d51625d7f4e592bb88bff9a1301
JongHun <pjh575157@gmail.com>
2023-03-11 14:29:09 +0900

    2023/03/11 게시물 관리를 위한 초기 더미 데이터 생성

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,6 @@
-insert into users values(90000, now(), 'user1', 'password1', 'ssn1');
-insert into users values(90001, now(), 'user2', 'password2', 'ssn2');
-insert into users values(90002, now(), 'user3', 'password3', 'ssn3');
+insert into users values(90001, now(), 'user1', 'password1', 'ssn1');
+insert into users values(90002, now(), 'user2', 'password2', 'ssn2');
+insert into users values(90003, now(), 'user3', 'password3', 'ssn3');
+
+insert into post values(100001, 'post1', 90001);
+insert into post values(100002, 'post2', 90002);
